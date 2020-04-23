1a829593f62d0bf24a8a15950c77c603f87076c4
Calvin Cheng <linchuan.cheng@gmail.com>
2020-04-23 11:57:38 +0800

    Remove unnecessary footer
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -48,13 +48,7 @@ const Home = () => (
     </main>
 
     <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
+
     </footer>
 
   </div>
