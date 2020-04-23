be1297db35895582c7f3be1df1cdb1ea7a8722a5
Calvin Cheng <linchuan.cheng@gmail.com>
2020-04-23 13:50:10 +0800

    No base url
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? '/docs/' : '',
+  assetPrefix: !debug ? '' : '',
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
